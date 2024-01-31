1cc77a7e107f977276f4bc8b9ef93004530b6b0b
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-01-30 21:55:29 -0300

    adiciona nova tela

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import ListPets from '../views/ListPets.vue'
+import FormPet from '../views/FormPet.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/pets/:id',
       name: 'ListPets',
       component: ListPets
+    },
+    {
+      path: '/pets/novo',
+      name: 'FormPet',
+      component: FormPet
     }
   ]
 })
