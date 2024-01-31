fa9af67a6b6e18ade7fcde48dc1b282dbc312734
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-01-31 20:48:16 -0300

    aplica correção do schema

--- src/validations/pet.validations.js
+++ src/validations/pet.validations.js
@@ -5,9 +5,9 @@ export const schemaPetForm = yup.object().shape({
     .string()
     .required("O nome é obrigatório")
     .max(150, 'O nome é muito longo'),
-    age: yup.integer("A idade deve ser um valor inteiro"),
-    race_id: yup.integer().required("A raça é obrigatória"),
-    specie_id: yup.integer().required("A espécie é obrigatória"),
+    age: yup.number().integer("A idade deve ser um valor inteiro"),
+    race_id: yup.number().integer().required("A raça é obrigatória"),
+    specie_id: yup.number().integer().required("A espécie é obrigatória"),
     weight: yup.number("O valor deve ser um número").required("o peso é obrigatório"),
     size: yup.string().required("O tamanho é obrigatório")
 }) 
