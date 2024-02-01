4579b7610bf365417893c6986e414d91cf4fa07a
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-01-31 21:15:32 -0300

    adiciona validacao e finalização do cadastro de pet

--- src/validations/pet.validations.js
+++ src/validations/pet.validations.js
@@ -6,8 +6,8 @@ export const schemaPetForm = yup.object().shape({
     .required("O nome é obrigatório")
     .max(150, 'O nome é muito longo'),
     age: yup.number().integer("A idade deve ser um valor inteiro"),
-    race_id: yup.number().integer().required("A raça é obrigatória"),
-    specie_id: yup.number().integer().required("A espécie é obrigatória"),
+    race_id: yup.string().required("A raça é obrigatória"),
+    specie_id: yup.string().required("A espécie é obrigatória"),
     weight: yup.number("O valor deve ser um número").required("o peso é obrigatório"),
     size: yup.string().required("O tamanho é obrigatório")
 }) 
--- src/views/FormPet.vue
+++ src/views/FormPet.vue
@@ -7,13 +7,30 @@
     <v-card width="80%" class="mx-auto px-6 mt-4" title="Cadastro de pet">
       <v-row>
         <v-col cols="12" md="8">
-          <v-text-field label="Nome" variant="outlined" v-model="name" />
+          <v-text-field
+            label="Nome"
+            variant="outlined"
+            v-model="name"
+            :error-messages="errors.name"
+          />
         </v-col>
         <v-col cols="12" md="2" sm="6">
-          <v-text-field label="Idade" type="number" variant="outlined" v-model="age" />
+          <v-text-field
+            label="Idade"
+            type="number"
+            variant="outlined"
+            v-model="age"
+            :error-messages="errors.age"
+          />
         </v-col>
         <v-col cols="12" md="2" sm="6">
-          <v-text-field label="Peso" type="number" variant="outlined" v-model="weight" />
+          <v-text-field
+            label="Peso"
+            type="number"
+            variant="outlined"
+            v-model="weight"
+            :error-messages="errors.weight"
+          />
         </v-col>
       </v-row>
 
@@ -25,6 +42,7 @@
             variant="outlined"
             placeholder="Selecione um item"
             v-model="size"
+            :error-messages="errors.size"
           />
         </v-col>
         <v-col cols="12" md="4">
@@ -33,9 +51,10 @@
             :items="itemsSpecies"
             variant="outlined"
             placeholder="Selecione um espécie"
-            v-model="specie"
+            v-model="specie_id"
             item-title="name"
             item-value="id"
+            :error-messages="errors.specie_id"
           />
         </v-col>
         <v-col cols="12" md="4">
@@ -44,9 +63,10 @@
             :items="itemsRaces"
             variant="outlined"
             placeholder="Selecione um raça"
-            v-model="race"
+            v-model="race_id"
             item-title="name"
             item-value="id"
+            :error-messages="errors.race_id"
           />
         </v-col>
       </v-row>
@@ -65,7 +85,9 @@ import SpecieService from '../services/SpecieService'
 import RaceService from '../services/RaceService'
 import PetService from '../services/PetService'
 
-import {schemaPetForm} from '../validations/pet.validations'
+import { schemaPetForm } from '../validations/pet.validations'
+import { captureErrorYup } from '../utils/captureErrorsYup'
+import * as yup from 'yup'
 
 export default {
   data() {
@@ -74,13 +96,14 @@ export default {
       age: 1,
       weight: 1,
       size: '',
-      specie: '',
-      race: '',
+      specie_id: '',
+      race_id: '',
 
       itemsSize: optionsSize,
       itemsSpecies: [],
       itemsRaces: [],
-      success: false
+      success: false,
+      errors: {}
     }
   },
   mounted() {
@@ -96,26 +119,38 @@ export default {
   },
   methods: {
     handleSubmit() {
-      PetService.createPet({
-        name: this.name,
-        age: this.age,
-        size: this.size,
-        race_id: this.race,
-        specie_id: this.specie,
-        weight: this.weight
-      }).then(() => {
-        this.success = true
+      try {
+        const body = {
+          name: this.name,
+          age: this.age,
+          size: this.size,
+          race_id: this.race_id,
+          specie_id: this.specie_id,
+          weight: this.weight
+        }
 
-        this.name = ''
-        this.age = 1
-        this.weight = 1
-        this.size = ''
-        this.specie = ''
-        this.race = ''
-      })
-      .catch(() => {
-        alert("Houve um erro ao cadastrar")
-      })
+        schemaPetForm.validateSync(body, { abortEarly: false })
+
+        PetService.createPet(body)
+          .then(() => {
+            this.success = true
+
+            this.name = ''
+            this.age = 1
+            this.weight = 1
+            this.size = ''
+            this.specie_id = ''
+            this.race_id = ''
+          })
+          .catch(() => {
+            alert('Houve um erro ao cadastrar')
+          })
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          this.errors = captureErrorYup(error)
+        }
+        // captura os erros lançados pela validação
+      }
     }
   }
 }
